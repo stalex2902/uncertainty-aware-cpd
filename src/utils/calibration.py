74d77643df052b0b49a890f0abcec7287e96da36
Alexander Stepikin <st.alexandr_2902@mail.ru>
2024-05-16 10:34:34 +0000

    update calibration

--- src/utils/calibration.py
+++ src/utils/calibration.py
@@ -5,11 +5,59 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from betacal import BetaCalibration
 from sklearn.calibration import calibration_curve
 from src.metrics.metrics_utils import collect_model_predictions_on_set
 from torch import nn, optim
 from torch.nn import functional as F
 from tqdm import tqdm
+
+
+class _ECELoss(nn.Module):
+    """
+    Calculates the Expected Calibration Error of a model.
+    (This isn't necessary for temperature scaling, just a cool metric).
+
+    The input to this loss is the logits of a model, NOT the softmax scores.
+
+    This divides the confidence outputs into equally-sized interval bins.
+    In each bin, we compute the confidence gap:
+
+    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
+
+    We then return a weighted average of the gaps, based on the number
+    of samples in each bin
+
+    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
+    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
+    2015.
+    """
+
+    def __init__(self, n_bins=15):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(_ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
 
 
 class ModelWithTemperature(nn.Module):
@@ -25,7 +73,6 @@ class ModelWithTemperature(nn.Module):
         super(ModelWithTemperature, self).__init__()
         self.model = model
         self.temperature = nn.Parameter(torch.ones(1) * 1.5)
-        self.verbose = verbose
         self.preprocessor = preprocessor
         self.lr = lr
         self.max_iter = max_iter
@@ -54,7 +101,7 @@ class ModelWithTemperature(nn.Module):
         return logits / temperature
 
     # This function probably should live outside of this class, but whatever
-    def set_temperature(self, valid_loader):
+    def fit(self, valid_loader):
         """
         Tune the tempearature of the model (using the validation set).
         We're going to set it to optimize NLL.
@@ -132,52 +179,73 @@ class ModelWithTemperature(nn.Module):
 
         return self
 
+    def predict(self, dataloader, model_type="seq2seq", device="cpu", verbose=False):
+        test_out_bank, _, test_labels_bank = collect_model_predictions_on_set(
+            self.model,
+            dataloader,
+            model_type=model_type,
+            device=device,
+            verbose=verbose,
+        )
+        preds_cal_flat = torch.vstack(test_out_bank).flatten()
+        labels_flat = torch.vstack(test_labels_bank).flatten()
 
-class _ECELoss(nn.Module):
+        return preds_cal_flat, labels_flat
+
+
+class ModelBeta:
     """
-    Calculates the Expected Calibration Error of a model.
-    (This isn't necessary for temperature scaling, just a cool metric).
-
-    The input to this loss is the logits of a model, NOT the softmax scores.
-
-    This divides the confidence outputs into equally-sized interval bins.
-    In each bin, we compute the confidence gap:
-
-    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
-
-    We then return a weighted average of the gaps, based on the number
-    of samples in each bin
-
-    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
-    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
-    2015.
+    A thin decorator, which wraps a model with temperature scaling
+    model (nn.Module):
+        A classification neural network
+        NB: Output of the neural network should be the classification logits,
+            NOT the softmax (or log softmax)!
     """
 
-    def __init__(self, n_bins=15):
+    def __init__(self, model, parameters="abm", preprocessor=None, verbose=True):
+        super(ModelBeta, self).__init__()
+        self.model = model
+        self.preprocessor = preprocessor
+
+        self.calibrator = BetaCalibration(parameters)
+
+    # This function probably should live outside of this class, but whatever
+    def fit(self, dataoader, model_type="seq2seq", device="cuda", verbose=True):
         """
-        n_bins (int): number of confidence interval bins
+        Tune the tempearature of the model (using the validation set).
+        We're going to set it to optimize NLL.
+        valid_loader (DataLoader): validation set loader
         """
-        super(_ECELoss, self).__init__()
-        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
-        self.bin_lowers = bin_boundaries[:-1]
-        self.bin_uppers = bin_boundaries[1:]
+        test_out_bank, _, test_labels_bank = collect_model_predictions_on_set(
+            self.model,
+            dataoader,
+            model_type=model_type,
+            device=device,
+            verbose=verbose,
+        )
 
-    def forward(self, logits, labels):
-        softmaxes = F.softmax(logits, dim=1)
-        confidences, predictions = torch.max(softmaxes, 1)
-        accuracies = predictions.eq(labels)
+        test_out_flat = torch.vstack(test_out_bank).flatten().numpy()
+        test_labels_flat = torch.vstack(test_labels_bank).flatten().numpy()
 
-        ece = torch.zeros(1, device=logits.device)
-        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
-            # Calculated |confidence - accuracy| in each bin
-            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
-            prop_in_bin = in_bin.float().mean()
-            if prop_in_bin.item() > 0:
-                accuracy_in_bin = accuracies[in_bin].float().mean()
-                avg_confidence_in_bin = confidences[in_bin].mean()
-                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+        self.calibrator.fit(test_out_flat.reshape(-1, 1), test_labels_flat)
 
-        return ece
+        return self
+
+    def predict(self, dataloader, model_type="seq2seq", device="cpu", verbose=False):
+        test_out_bank, _, test_labels_bank = collect_model_predictions_on_set(
+            self.model,
+            dataloader,
+            model_type=model_type,
+            device=device,
+            verbose=verbose,
+        )
+
+        preds_flat = torch.vstack(test_out_bank).flatten()
+        labels_flat = torch.vstack(test_labels_bank).flatten()
+
+        preds_cal_flat = self.calibrator.predict(preds_flat)
+
+        return preds_cal_flat, labels_flat
 
 
 # ------------------------------------------------------------------------------------------------------------#
@@ -188,65 +256,92 @@ class _ECELoss(nn.Module):
 def calibrate_single_model(
     core_model,
     val_dataloader,
+    cal_type="beta",
+    parameters_beta="abm",
     preprocessor=None,
     lr=1e-2,
     max_iter=50,
     verbose=True,
     device="cpu",
-    return_loss=False,
+    # return_loss=False,
 ):
+    assert cal_type in ["temperature", "beta"], f"Unknown calibration type {cal_type}"
+
     core_model.to(device)
     if preprocessor:
         preprocessor.to(device)
 
-    core_model.return_logits = True
-    scaled_model = ModelWithTemperature(
-        core_model, preprocessor=preprocessor, lr=lr, max_iter=max_iter, verbose=verbose
-    )
-    scaled_model.set_temperature(val_dataloader)
-    core_model.return_logits = False
+    if cal_type == "temperature":
+        core_model.return_logits = True
+        scaled_model = ModelWithTemperature(
+            core_model,
+            preprocessor=preprocessor,
+            lr=lr,
+            max_iter=max_iter,
+        )
+        scaled_model.fit(val_dataloader, verbose=verbose)
+        core_model.return_logits = False
+
+    else:
+        scaled_model = ModelBeta(
+            core_model,
+            parameters=parameters_beta,
+            preprocessor=preprocessor,
+        )
+        scaled_model.fit(val_dataloader, verbose=verbose)
+
+    return scaled_model
 
     # print("Loss calibration hystory:", scaled_model.loss_history)
 
-    if return_loss:
-        return core_model, scaled_model.loss_history
-    else:
-        return core_model, None
+    # if return_loss:
+    #     return core_model, scaled_model.loss_history
+    # else:
+    #     return core_model, None
+
+    # return scaled_model
 
 
 def calibrate_all_models_in_ensemble(
     ensemble_model,
     val_dataloader,
+    cal_type,
     preprocessor=None,
     lr=1e-2,
     max_iter=50,
     verbose=True,
     device="cpu",
 ):
+    cal_models = []
     for cpd_model in ensemble_model.models_list:
-        _ = calibrate_single_model(
+        cal_model = calibrate_single_model(
             cpd_model.model,
             val_dataloader,
+            cal_type,
             preprocessor=preprocessor,
             lr=lr,
             max_iter=max_iter,
             verbose=verbose,
             device=device,
         )
+        cal_models.append(cal_model)
+    return cal_models
 
 
-def manually_calibrate_all_models_in_ensemble(ensemble_model, temperature_list):
+def manually_temperature_calibrate_all_models_in_ensemble(
+    ensemble_model, temperature_list
+):
     for cpd_model, T in zip(ensemble_model.models_list, temperature_list):
         cpd_model.model.temperature = T
 
 
-def uncalibrate_all_models_in_ensemble(ensemble_model):
+def temperature_uncalibrate_all_models_in_ensemble(ensemble_model):
     for cpd_model in ensemble_model.models_list:
         cpd_model.model.temperature = 1.0
 
 
 def plot_calibration_curves(
-    models_list,
+    cal_models_list,
     test_dataloader,
     model_type="seq2seq",
     device="cpu",
@@ -258,17 +353,10 @@ def plot_calibration_curves(
     plt.figure(figsize=(10, 8))
     plt.plot(x_ideal, x_ideal, linestyle="--", label="Ideally calibrated", c="black")
 
-    for i, cpd_model in enumerate(models_list):
-        test_out_bank, _, test_labels_bank = collect_model_predictions_on_set(
-            cpd_model,
-            test_dataloader,
-            model_type=model_type,
-            device=device,
-            verbose=verbose,
+    for i, cal_model in enumerate(cal_models_list):
+        test_out_flat, test_labels_flat = cal_model.predict(
+            test_dataloader, model_type=model_type, device=device, verbose=verbose
         )
-
-        test_out_flat = torch.vstack(test_out_bank).flatten()
-        test_labels_flat = torch.vstack(test_labels_bank).flatten()
 
         prob_true, prob_pred = calibration_curve(
             test_labels_flat, test_out_flat, n_bins=10
